1590f92d424f1859b302fddd7cfb788f390e85d2
hadil <hadil.ibnhajfraj@esprit.tn>
2025-03-08 21:33:33 +0100

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import * as Chartist from 'chartist';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+}
